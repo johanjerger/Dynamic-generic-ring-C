ac23e34e938258ef8aa6e41c41207803f3573cd1
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-10 14:53:02 -0300

    Added new and size methods

--- src/dynamic_generic_ring.c
+++ src/dynamic_generic_ring.c
@@ -1,1 +1,15 @@
+#include <stdlib.h>
 #include "includes/dynamic_generic_ring.h"
+#include "includes/dgr_operations.h"
+
+ring_t * new_ring()
+{
+        ring_t * new_ring;
+
+        new_ring = malloc(sizeof(ring_t));
+        new_ring->next = new_ring;
+        new_ring->previous = new_ring;
+        new_ring->size = &dgr_size;
+
+        return new_ring;
+}
