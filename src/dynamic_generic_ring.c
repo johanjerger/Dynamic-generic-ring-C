97f0e9ebb42b754d274c96339aba3702b0498a71
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-10 15:15:40 -0300

    Beautyfied

--- src/dynamic_generic_ring.c
+++ src/dynamic_generic_ring.c
@@ -2,6 +2,10 @@
 #include "includes/dynamic_generic_ring.h"
 #include "includes/dgr_operations.h"
 
+/*
+ * Here I define the interface of the ring_t structure.
+ */
+
 ring_t * new_ring()
 {
         ring_t * new_ring;
@@ -9,6 +13,9 @@ ring_t * new_ring()
         new_ring = malloc(sizeof(ring_t));
         new_ring->next = new_ring;
         new_ring->previous = new_ring;
+
+        // Ring functions assignation
+
         new_ring->size = &dgr_size;
 
         return new_ring;
