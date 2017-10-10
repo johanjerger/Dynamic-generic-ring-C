909f76a2309b4fbff72341673ef7bc19ec53e97f
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-10 15:15:33 -0300

    Beautyfied

--- src/dgr_operations.c
+++ src/dgr_operations.c
@@ -1,11 +1,15 @@
 #include "includes/dynamic_generic_ring.h"
 
+/*
+ *  Here I define all the ring functions.
+ */
 
 int dgr_size(ring_t * act_ring)
 {
         int size = 1;
         ring_t * head = act_ring;
 
+        // while you don't walk in circles...
         while(head->next != act_ring) {
                 size++;
                 head = head->next;
