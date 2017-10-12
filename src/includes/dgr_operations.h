f91cf82866a81028d914d66817637fbe845ceed6
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-12 11:38:28 -0300

    Desacomplated method add from this file

--- src/includes/dgr_operations.h
+++ src/includes/dgr_operations.h
@@ -2,11 +2,10 @@
 #define DGR_OPR_H
 
 /*
- *  Here I define all the ring functions headers.
+ *  Here I define all the ring generic functions headers.
  */
 
     int dgr_size(ring_t *);
-    ring_t * dgr_add(ring_t *, void *);
     bool dgr_exist(ring_t *, void *);
     void dgr_destruct(ring_t *);
 
