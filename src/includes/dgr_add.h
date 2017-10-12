27707a3b4566f80c9bcf778fba64a2de310eeaec
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-12 17:29:18 -0300

    Changed the return type of the functions

--- src/includes/dgr_add.h
+++ src/includes/dgr_add.h
@@ -5,8 +5,9 @@
  *  Here I define all the ring add functions headers.
  */
 
-    ring_t * dgr_add(ring_t *, void *);
-    ring_t * dgr_add_before(ring_t *, void *);
-    ring_t * dgr_add_next(ring_t *, void *);
+    void dgr_add_elem(ring_t *, void *);
+    void dgr_add(ring_t *, void *);
+    void dgr_add_before(ring_t *, void *);
+    void dgr_add_next(ring_t *, void *);
 
 #endif
