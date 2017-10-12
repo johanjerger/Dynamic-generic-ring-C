bba850d41afc877af443e9df9c3c6261145fd19c
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-12 13:13:24 -0300

    Changed add for add_before and add_next

--- src/dgr_add.c
+++ src/dgr_add.c
@@ -7,8 +7,9 @@
  */
 
 // The add function add an element on before the actual ring position.
-ring_t * dgr_add(ring_t * act_ring, void * elem)
+ring_t * dgr_add_before(ring_t * act_ring, void * elem)
 {
+        act_ring->size++;
         if (act_ring->elem == NULL) {
                 return (act_ring->elem = elem);
         }
@@ -22,4 +23,29 @@ ring_t * dgr_add(ring_t * act_ring, void * elem)
         act_ring->previous = ring;
 
         return act_ring;
+}
+
+// The add function add an element on after the actual ring position.
+ring_t * dgr_add_next(ring_t * act_ring, void * elem)
+{
+        act_ring->size++;
+        if (act_ring->elem == NULL) {
+                return (act_ring->elem = elem);
+        }
+
+        ring_t * ring = new_ring();
+
+        ring->elem = elem;
+        ring->previous = act_ring;
+        ring->next = act_ring->next;
+        act_ring->next->previous = ring;
+        act_ring->next = ring;
+
+        return act_ring;
+}
+
+// This method is a wrapper of dgr_add_before
+ring_t * dgr_add(ring_t * act_ring, void * elem)
+{
+        return act_ring->add_before(act_ring, elem);
 }
