844dc5bae46ea7345a3238897845b1673ce1d0de
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-12 17:28:43 -0300

    Added more delete functions

--- src/dgr_delete.c
+++ src/dgr_delete.c
@@ -6,16 +6,23 @@
  *  Here I define all the ring add functions.
  */
 
-// The delete function delete an element on before the actual ring position.
-ring_t * dgr_delete_before(ring_t * act_ring)
+static bool dgr_empty_delete(ring_t * act_ring)
 {
         if (act_ring->size == 0) {
-                return act_ring;
+                return true;
         }
         if (act_ring->size == 1) {
                 act_ring->size--;
-                return act_ring->elem = NULL;
+                act_ring->elem = NULL;
+                return true;
         }
+        return false;
+}
+
+// The delete function delete an element on before the actual ring position.
+void dgr_delete_before(ring_t * act_ring)
+{
+        if(dgr_empty_delete(act_ring)) return;
 
         act_ring->size--;
 
@@ -23,20 +30,12 @@ ring_t * dgr_delete_before(ring_t * act_ring)
         new_previous->next = act_ring;
         free(act_ring->previous);
         act_ring->previous = new_previous;
-
-        return act_ring;
 }
 
 // The delete function delete an element on after the actual ring position.
-ring_t * dgr_delete_next(ring_t * act_ring)
+void dgr_delete_next(ring_t * act_ring)
 {
-        if (act_ring->size == 0) {
-                return act_ring;
-        }
-        if (act_ring->size == 1) {
-                act_ring->size--;
-                return act_ring->elem = NULL;
-        }
+        if(dgr_empty_delete(act_ring)) return;
 
         act_ring->size--;
 
@@ -44,76 +43,69 @@ ring_t * dgr_delete_next(ring_t * act_ring)
         new_next->previous = act_ring;
         free(act_ring->next);
         act_ring->next = new_next;
-
-        return act_ring;
 }
 
 // This function deletes a particular elem in the ring
-ring_t * dgr_delete_elem(ring_t * act_ring, void * elem)
+void dgr_delete_elem(ring_t * act_ring, void * elem)
 {
         ring_t * head = act_ring;
 
         while(head->next != act_ring) {
                 if(head->equals_callback(head->elem, elem)) {
                         head->delete(head->next);
-                        return act_ring;
+                        return;
                 }
                 head = head->next;
         }
-
-        return act_ring;
 }
 
 
 // This function deletes all the elems that are like your elem
-ring_t * dgr_delete_all_elem(ring_t * act_ring, void * elem)
+void dgr_delete_all_elem(ring_t * act_ring, void * elem)
 {
         ring_t * head = act_ring;
 
         while(head->next != act_ring) {
                 if(head->equals_callback(head->elem, elem)) {
                         head->delete(head->next);
-                        return act_ring;
                 }
                 head = head->next;
         }
-
-        return act_ring;
 }
 
-// This function deletes the next elem after the elem like you pass
-ring_t * dgr_delete_next_elem(ring_t * act_ring, void * elem)
+// This function deletes the first ocurrensy next elem after the elem
+// like you pass
+void dgr_delete_next_elem_to(ring_t * act_ring, void * elem)
 {
         ring_t * head = act_ring;
 
         while(head->next != act_ring) {
                 if(head->equals_callback(head->elem, elem)) {
                         head->delete_next(head);
+                        return;
                 }
                 head = head->next;
         }
-
-        return act_ring;
 }
 
-// This function deletes the before elem after the elem like you pass
-ring_t * dgr_delete_before_elem(ring_t * act_ring, void * elem)
+// This function deletes the first ocurrensy before elem after the elem
+// like you pass
+void dgr_delete_before_elem_to(ring_t * act_ring, void * elem)
 {
         ring_t * head = act_ring;
 
         while(head->next != act_ring) {
                 if(head->equals_callback(head->elem, elem)) {
                         head->delete_before(head);
+                        return;
                 }
                 head = head->next;
         }
-
-        return act_ring;
 }
 
 
 // This method is a wrapper of dgr_delete_before
-ring_t * dgr_delete(ring_t * act_ring)
+void dgr_delete(ring_t * act_ring)
 {
-        return act_ring->delete_before(act_ring);
+        act_ring->delete_before(act_ring);
 }
