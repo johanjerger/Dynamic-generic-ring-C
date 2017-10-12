db87750931952de37103151cab44dc52c5031d4b
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-12 13:14:05 -0300

    Changed ring structure

--- src/dynamic_generic_ring.c
+++ src/dynamic_generic_ring.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include "includes/dynamic_generic_ring.h"
 #include "includes/dgr_operations.h"
+#include "includes/dgr_callbacks.h"
 #include "includes/dgr_add.h"
 
 /*
@@ -14,13 +15,20 @@ ring_t * new_ring()
 
         new_ring = (ring_t *) malloc(sizeof(ring_t));
         new_ring->elem = NULL;
+        new_ring->size = 0;
         new_ring->next = new_ring;
         new_ring->previous = new_ring;
 
         // Ring functions assignation
 
-        new_ring->size = &dgr_size;
         new_ring->add = &dgr_add;
+        new_ring->add_before = &dgr_add_before;
+        new_ring->add_next = &dgr_add_next;
+        new_ring->exist = &dgr_exist;
+
+        // Callbacks
+
+        new_ring->set_equals_callback = &dgr_set_equals_callback;
 
         return new_ring;
 }
