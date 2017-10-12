8e4a848f0c2b78460e29dea88fe55862f29a1531
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-12 15:44:59 -0300

    Added destruct function assignation

--- src/dynamic_generic_ring.c
+++ src/dynamic_generic_ring.c
@@ -4,6 +4,7 @@
 #include "includes/dgr_operations.h"
 #include "includes/dgr_callbacks.h"
 #include "includes/dgr_add.h"
+#include "includes/dgr_delete.h"
 
 /*
  * Here I define the interface of the ring_t structure.
@@ -21,15 +22,20 @@ ring_t * new_ring()
 
         // Ring functions assignation
 
-        new_ring->get_elem = &dgr_get_elem;
         new_ring->add = &dgr_add;
         new_ring->add_before = &dgr_add_before;
         new_ring->add_next = &dgr_add_next;
         new_ring->exist = &dgr_exist;
+        new_ring->delete_before = &dgr_delete_before;
+        new_ring->delete = &dgr_delete;
 
         // Callbacks
 
         new_ring->set_equals_callback = &dgr_set_equals_callback;
+
+        // Destroy functions
+
+        new_ring->destruct = &dgr_destruct;
 
         return new_ring;
 }
