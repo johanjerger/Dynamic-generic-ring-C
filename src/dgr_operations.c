83c494d121e1e91f7c22f17d1641ba4622a4e512
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-12 13:13:39 -0300

    Delete size function

--- src/dgr_operations.c
+++ src/dgr_operations.c
@@ -6,38 +6,19 @@
  *  Here I define all the ring generic functions.
  */
 
-// The size function return the number of ring structs linked.
-int dgr_size(ring_t * act_ring)
-{
-        if (act_ring->elem == NULL) {
-                return 0;
-        }
-
-        int size = 1;
-        ring_t * head = act_ring;
-
-        // while you don't walk in circles...
-        while(head->next != act_ring) {
-                size++;
-                head = head->next;
-        }
-
-        return size;
-}
-
 // Checks if an elem exist in the ring
 bool dgr_exist(ring_t * act_ring, void * elem)
 {
         ring_t * head = act_ring;
 
         while(head->next != act_ring) {
-                if(act_ring->exist_callback(head->elem, elem)) {
+                if(act_ring->equals_callback(head->elem, elem)) {
                         return true;
                 }
                 head = head->next;
         }
 
-        return false;
+        return act_ring->equals_callback(head->elem, elem);
 }
 
 // Free all the allocated memory
