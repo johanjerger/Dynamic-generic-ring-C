730e6b523b6154fdc84590f4365b7e00d211111a
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-12 13:16:32 -0300

    Callbacks h file created

---
+++ src/includes/dgr_callbacks.h
@@ -0,0 +1,10 @@
+#ifndef DGR_CB_H
+#define DGR_CB_H
+
+/*
+ *  Here I define all the ring generic functions headers.
+ */
+
+    void dgr_set_equals_callback(ring_t * act_ring, bool (*)(void *, void *));
+
+#endif
