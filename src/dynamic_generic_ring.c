edba5ea42886352c815fd9b75db13faf1f2653d2
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-20 09:20:26 -0300

    Seted size and print funtions

--- src/dynamic_generic_ring.c
+++ src/dynamic_generic_ring.c
@@ -16,7 +16,6 @@ ring_t * new_ring()
 
         new_ring = (ring_t *) malloc(sizeof(ring_t));
         new_ring->elem = NULL;
-        new_ring->size = 0;
         new_ring->next = new_ring;
         new_ring->previous = new_ring;
 
@@ -27,6 +26,8 @@ ring_t * new_ring()
         new_ring->add_before = &dgr_add_before;
         new_ring->add_next = &dgr_add_next;
         new_ring->exist = &dgr_exist;
+        new_ring->print = &dgr_print;
+        new_ring->size = &dgr_size;
         new_ring->delete_before = &dgr_delete_before;
         new_ring->delete = &dgr_delete;
         new_ring->delete_next = &dgr_delete_next;
@@ -38,6 +39,7 @@ ring_t * new_ring()
         // Callbacks
 
         new_ring->set_equals_callback = &dgr_set_equals_callback;
+        new_ring->set_print_callback = &dgr_set_print_callback;
 
         // Destroy functions
 
