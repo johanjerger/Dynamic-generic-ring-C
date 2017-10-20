9793e86308679a98b78894aef75b4313995747cb
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-20 09:19:43 -0300

    Added print callback

--- src/dgr_callbacks.c
+++ src/dgr_callbacks.c
@@ -13,3 +13,15 @@ void dgr_set_equals_callback(ring_t * act_ring, bool (*equals_callback)(void *, 
 
         head->equals_callback = equals_callback;
 }
+
+void dgr_set_print_callback(ring_t * act_ring, void (*print_callback)(void *))
+{
+        ring_t * head = act_ring;
+
+        while(head->next != act_ring) {
+                head->print_callback = print_callback;
+                head = head->next;
+        }
+
+        head->print_callback = print_callback;
+}
